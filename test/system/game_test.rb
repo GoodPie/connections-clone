80ad128908cd4d353d42082bff4850eecba871ca
Brandyn Britton <Brandynbb96@gmail.com>
2024-01-24 12:35:24 +0800

    update: created basic test fixture data

--- test/system/game_test.rb
+++ test/system/game_test.rb
@@ -3,8 +3,14 @@ require "application_system_test_case"
 class GameTest < ApplicationSystemTestCase
 
   test "Start Game" do
+
+    Rails.application.load_seed
     visit root_url
-    click_on "Start Game"
+
+    # Click on the big button to play the game
+    click_on "Play"
+
+    # Title is still visible
     assert_selector "h1", text: "Connections Clone"
   end
 
